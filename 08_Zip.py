895e471d4740feb4c112bccabf0ee24cef0eab44
highslater <highslater@hotmail.com>
2019-03-19 00:06:43 -0400

    commit_08

---
+++ 08_Zip.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3.7
+
+"""08_Zip.py.
+
+Eighth Program of the Sentdex Intermediate Python Series.
+
+"""
+import logging
+
+
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+
+logging.basicConfig(filename="LOG_files/LOG_08.Log",
+                    level=logging.DEBUG,
+                    format=LOG_FORMAT,
+                    filemode='w')
+logger = logging.getLogger()
+logger.info("08_Zip.py RUN / START")
+
+
+x = [1, 2, 3, 4]
+y = [7, 6, 2, 1]
+z = list("abcd")
+
+for a, b in zip(x, y):
+    print(a, b, end=", ")
+print()
+
+for i in zip(x, y, z):
+    print(i, end=", ")
+print()
+
+xx = [i for i in range(1, 11)]
+yy = [i ** 2 for i in range(1, 11)]
+xxyy = dict(zip(xx, yy))
+print(xxyy)
