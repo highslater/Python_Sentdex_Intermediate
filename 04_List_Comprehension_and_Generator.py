dd45af29515497954cb2f071526bd4c5fb400d42
highslater <highslater@hotmail.com>
2019-03-18 20:05:23 -0400

    commit

--- 04_List_Comprehension_and_Generator.py
+++ 04_List_Comprehension_and_Generator.py
@@ -2,7 +2,7 @@
 
 """04_List_Comprehension_and_Generator.py.
 
-ZFourth Program of the Sentdex Intermediate Python Series.
+Fourth Program of the Sentdex Intermediate Python Series.
 
 """
 import logging
@@ -16,3 +16,57 @@ logging.basicConfig(filename="LOG_files/LOG_04.Log",
                     filemode='w')
 logger = logging.getLogger()
 logger.info("04_List_Comprehension_and_Generator.py RUN / START")
+
+list_1 = [i for i in range(5)]
+
+list_2 = []
+for i in range(5):
+    list_2.append(i)
+
+print("*" * 90)
+print(list_1 == list_2)
+print("*" * 90)
+
+g_1 = (i for i in range(10))
+
+print("*" * 90)
+for i in g_1:
+    print(i, end=", ")
+
+print("11111")
+g_1 = (i for i in range(10))
+[print(i) for i in g_1]
+print("11111")
+
+print()
+print("*" * 90)
+
+
+def gen():
+    """Docstring."""
+    for x in range(10):
+        yield x
+
+g_2 = gen()
+
+print("*" * 90)
+for i in g_2:
+    print(i, end=", ")
+
+print("22222")
+[print(i) for i in gen()]
+print("22222")
+
+print()
+print("*" * 90)
+
+print("*" * 90)
+print(g_1)
+print(g_2)
+print(g_1 == g_2)
+print("*" * 90)
+
+print("*" * 90)
+print(list_1)
+print(g_1)
+print("*" * 90)
---
+++ 05_More_List_Comprehension_and_Generator.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3.7
+
+"""05_More_List_Comprehension_and_Generator.py.
+
+Fifth Program of the Sentdex Intermediate Python Series.
+
+"""
+import logging
+
+
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+
+logging.basicConfig(filename="LOG_files/LOG_05.Log",
+                    level=logging.DEBUG,
+                    format=LOG_FORMAT,
+                    filemode='w')
+logger = logging.getLogger()
+logger.info("05_More_List_Comprehension_and_Generator.py RUN / START")
+
+
+input_list = [5, 6, 2, 10, 15, 20, 5, 2, 1, 3]
+
+'''
+def div_by_five(num):
+    """Docstring."""
+    if num % 5 == 0:
+        return True
+    else:
+        return False
+'''
+
+
+def div_by_five(num):
+    """Docstring."""
+    return num % 5 == 0
+
+
+xyz = (i for i in input_list if div_by_five(i))
+print("\n", "*" * 90)
+for i in xyz:
+    print("", i, end="+ ")
+print("\n", "*" * 90)
+
+xyz = (i for i in input_list if div_by_five(i))
+print("\n", "*" * 90)
+[print("", i, end="- ") for i in xyz]
+print("\n", "*" * 90)
+
+"""
+you can not iterate over a generator twice.
+a generator is exhausted once you have iterated over it.
+
+"""
+
+print("\n", "*" * 95)
+[[print("", (i, ii), end=", ") for ii in range(3)] for i in range(3)]
+print("\n", "*" * 95)
+
+xyz = ([[i, ii] for ii in range(5)] for i in range(5))
+
+print("\n", "*" * 95)
+print([i for i in xyz])
+print("\n", "*" * 95)
+
+xyz = ([(i, ii) for ii in range(5)] for i in range(5))
+print("\n", "*" * 95)
+for i in xyz:
+    print(i)
+print("\n", "*" * 95)
+
+xyz = (((i, ii) for ii in range(5)) for i in range(5))
+print("\n", "*" * 95)
+for i in xyz:
+    for ii in i:
+        print(ii)
+print("\n", "*" * 95)
+
+xyz = (print("", i, end=",") for i in range(5))
+print("\n", "*" * 95)
+for i in xyz:
+    i
+print("\n", "*" * 95)
