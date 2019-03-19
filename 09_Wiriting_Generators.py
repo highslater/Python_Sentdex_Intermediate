c7327ef633856ba43c3169eb6e869102aa9a7d29
highslater <highslater@hotmail.com>
2019-03-19 14:14:40 -0400

    commit

--- 09_Wiriting_Generators.py
+++ 09_Wiriting_Generators.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3.7
 
-"""00_template.py.
+"""09_Wiriting_Generators.py.
 
-Zeroeth Program of the Sentdex Intermediate Python Series.
+Ninth Program of the Sentdex Intermediate Python Series.
 
 """
-
 import logging
 from platform import python_version
 from sys import hexversion
 
-VERSION_INFO = True
+PRINT_VERSION_INFO = True
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+
 version_info = "The Python Version is: {}  #{}\n".format(
     python_version(), str((hexversion)))
 
-LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+[print(x, end="") for x in version_info if PRINT_VERSION_INFO]
 
-[print(x, end="") for x in version_info if VERSION_INFO]
-
-logging.basicConfig(filename="LOG_files/LOG_00.Log",
+logging.basicConfig(filename="LOG_files/LOG_09.Log",
                     level=logging.DEBUG,
                     format=LOG_FORMAT,
                     filemode='w')
 logger = logging.getLogger()
 logger.info(version_info)
-logger.info("00_template.py RUN / START")
+logger.info("09_Wiriting_Generators.py RUN / START")
