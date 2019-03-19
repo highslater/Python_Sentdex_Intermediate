f39a68b5533ea14f26c5fce104ffecfa24aa793b
highslater <highslater@hotmail.com>
2019-03-19 19:48:12 -0400

    commit

--- 00_template.py
+++ 00_template.py
@@ -5,23 +5,19 @@
 Zeroeth Program of the Sentdex Intermediate Python Series.
 
 """
-
 import logging
 from platform import python_version
 from sys import hexversion
 
-VERSION_INFO = True
-version_info = "The Python Version is: {}  #{}\n".format(
-    python_version(), str((hexversion)))
-
+PRINT_VERSION_INFO = True
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
-
-[print(x, end="") for x in version_info if VERSION_INFO]
-
+version_info = "The Python Version is: {}  #{}".format(
+    python_version(), str((hexversion)))
 logging.basicConfig(filename="LOG_files/LOG_00.Log",
-                    level=logging.DEBUG,
-                    format=LOG_FORMAT,
+                    level=logging.DEBUG, format=LOG_FORMAT,
                     filemode='w')
 logger = logging.getLogger()
-logger.info(version_info)
+[logger.debug(version_info), None][PRINT_VERSION_INFO]
 logger.info("00_template.py RUN / START")
+
+[print(version_info), None][PRINT_VERSION_INFO]
--- 09_Wiriting_Generators.py
+++ 09_Wiriting_Generators.py
@@ -11,16 +11,25 @@ from sys import hexversion
 
 PRINT_VERSION_INFO = True
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
-
-version_info = "The Python Version is: {}  #{}\n".format(
+version_info = "The Python Version is: {}  #{}".format(
     python_version(), str((hexversion)))
-
-[print(x, end="") for x in version_info if PRINT_VERSION_INFO]
-
 logging.basicConfig(filename="LOG_files/LOG_09.Log",
-                    level=logging.DEBUG,
-                    format=LOG_FORMAT,
+                    level=logging.DEBUG, format=LOG_FORMAT,
                     filemode='w')
 logger = logging.getLogger()
-logger.info(version_info)
+[logger.debug(version_info), None][PRINT_VERSION_INFO]
 logger.info("09_Wiriting_Generators.py RUN / START")
+
+[print(version_info), None][PRINT_VERSION_INFO]
+
+
+def simple_gen():
+    """Docstring."""
+    yield("Oh")
+    yield("Hello")
+    yield("There")
+
+for i in simple_gen():
+    print(i, end=" ")
+
+print()
