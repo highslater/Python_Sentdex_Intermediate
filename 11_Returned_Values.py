4f9469b811a3c457a6751ac7cd8240c22b03b33f
highslater <highslater@hotmail.com>
2019-03-20 15:31:23 -0400

    commit_11

--- 11_Returned_Values.py
+++ 11_Returned_Values.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3.7
 
-"""10_Multiprocessing.py.
+"""11_Returned_Values.py.
 
-Tenth Program of the Sentdex Intermediate Python Series.
+Elevenenth Program of the Sentdex Intermediate Python Series.
 
 """
 import logging
@@ -15,12 +15,12 @@ PRINT_VERSION_INFO = True
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 version_info = "The Python Version is: {}  #{}".format(
     python_version(), str((hexversion)))
-logging.basicConfig(filename="LOG_files/LOG_10.Log",
+logging.basicConfig(filename="LOG_files/LOG_11.Log",
                     level=logging.DEBUG, format=LOG_FORMAT,
                     filemode='w')
 logger = logging.getLogger()
 [logger.debug(version_info), None][PRINT_VERSION_INFO]
-logger.info("10_Multiprocessing.py RUN / START")
+logger.info("11_Returned_Values.py RUN / START")
 
 [print(version_info), None][PRINT_VERSION_INFO]
 
