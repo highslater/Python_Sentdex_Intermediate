e9798d1881e02c7801ff01906882c8f65500a4c3
highslater <highslater@hotmail.com>
2019-03-20 22:16:38 -0400

    commit-12

--- 12_Spider_Example.py
+++ 12_Spider_Example.py
@@ -35,10 +35,64 @@ def random_starting_url():
         string.ascii_lowercase) for _ in range(3))
     return ''.join(['http://', starting, '.com'])
 
-# url = random_starting_url()
-# print(url)
-
 
 def handle_local_links(url, link):
     """Docstring."""
     return [''.join([url, link]), link][link.startswith('/')]
+
+
+def get_links(url):
+    """Docstring."""
+    try:
+        resp = requests.get(url)
+        soup = bs.BeautifulSoup(resp.text, 'lxml')
+        body = soup.body
+        links = [link.get('href') for link in body.find_all('a')]
+        links = [handle_local_links(url, link) for link in links]
+        links = [str(link.encode("ascii")) for link in links]
+        return links
+    except TypeError as e:
+        print(e)
+        # Log Error to file.
+        print("Got a TypeError, probably tried iterating over a None")
+        return []
+
+    except IndexError as e:
+        print(e)
+        # Log Error to file.
+        print("Got a IndexError, probably did not find any usefull links"
+              "Returning []")
+        return []
+
+    except AttributeError as e:
+        print(e)
+        # Log Error to file.
+        print("Got a AttributeError, probably tried iterating over a None")
+        return []
+
+    except Exception as e:
+        print(e)
+        # Log Error to file.
+        print("Got a General Exception, dunnno")
+        return []
+    else:
+        pass
+    finally:
+        pass
+
+
+def main():
+    """Docstring."""
+    how_many = 50
+    p = Pool(processes=how_many)
+    parse_us = [random_starting_url() for _ in range(how_many)]
+    data = p.map(get_links, [link for link in parse_us])
+    data = [url for url_list in data for url in url_list]
+    p.close()
+
+    with open('./Files/urls.txt', 'w') as f:
+        f.write(str(data))
+
+
+if __name__ == '__main__':
+    main()
