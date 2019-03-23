104ecbcea3486dd87f63c4a9d02f7c90e0e1096d
highslater <highslater@hotmail.com>
2019-03-23 15:41:43 -0400

    commit

--- 17SCRATCH_OOP_Introduction_5.py
+++ 17SCRATCH_OOP_Introduction_5.py
@@ -7,6 +7,7 @@ downgraded to accomodate pygame module.
 
 """
 import logging
+import random
 from platform import python_version
 from sys import hexversion
 import pygame
@@ -40,72 +41,63 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
-STARTING_BLUE_ELEMENTS = 10
-STARTING_RED_ELEMENTS = 10
-STARTING_GREEN_ELEMENTS = 10
-STARTING_WHITE_ELEMENTS = 10
+STARTING_BLUE_ELEMENTS = 1
+STARTING_RED_ELEMENTS = 1
+STARTING_GREEN_ELEMENTS = 1
+STARTING_WHITE_ELEMENTS = 1
 
 game_display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("~ Element World ~")
 clock = pygame.time.Clock()
 
 
-def make_dicts():
+def make_random_dict():
     """Docstring."""
-    red_elements = dict(
-        enumerate([Element(RED, WIDTH, HEIGHT)for i in range(
-            STARTING_RED_ELEMENTS)]))
-    blue_elements = dict(
-        enumerate([Element(BLUE, WIDTH, HEIGHT)for i in range(
-            STARTING_BLUE_ELEMENTS)]))
-    green_elements = dict(
-        enumerate([Element(GREEN, WIDTH, HEIGHT)for i in range(
-            STARTING_GREEN_ELEMENTS)]))
-    white_elements = dict(
-        enumerate([Element(WHITE, WIDTH, HEIGHT)for i in range(
-            STARTING_WHITE_ELEMENTS)]))
-    return [red_elements, blue_elements, green_elements, white_elements]
+    red_elements = list([Element(RED, WIDTH, HEIGHT)for i in range(
+        STARTING_RED_ELEMENTS)])
+    blue_elements = list([Element(BLUE, WIDTH, HEIGHT)for i in range(
+        STARTING_BLUE_ELEMENTS)])
+    green_elements = list([Element(GREEN, WIDTH, HEIGHT)for i in range(
+        STARTING_GREEN_ELEMENTS)])
+    white_elements = list([Element(WHITE, WIDTH, HEIGHT)for i in range(
+        STARTING_WHITE_ELEMENTS)])
+
+    el_list = red_elements + blue_elements + green_elements + white_elements
+    random.shuffle(el_list)
+    shuffled_dict = dict(enumerate(el_list))
+
+    return shuffled_dict
 
 
-def pygame_init(element_list):
+def pygame_init(random_dict):
     """Docstring."""
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        draw_environment(element_list)
+        draw_environment(random_dict)
         clock.tick(1)
 
 
-def draw_environment(element_list):
+def draw_environment(random_dict):
     """Docstring."""
-# Log v
-    logger.info('-' * 45)
-    logger.info(' {}\t{}\t {}\t{}'.format('#', '( x, y )', 'size', 'color'))
-    logger.info('-' * 45)
-# Log ^
     game_display.fill(BLACK)
-    for colored_elements in element_list:
-        for element_id in colored_elements:
-            element = colored_elements[element_id]
-# Log v
-            logger.debug('{} \t{} \t\t{} \t{}'.format(
-                element_id, (element.x, element.y),
-                element.size, element.color))
-# Log ^
-            pygame.draw.circle(game_display, element.color,
-                               [element.x, element.y], element.size)
-            element.move()
-            element.check_bounds()
+
+    for element_id in random_dict:
+        element = random_dict[element_id]
+        pygame.draw.circle(game_display, element.color,
+                           [element.x, element.y], element.size)
+        element.move()
+        element.check_bounds()
 
     pygame.display.update()
 
 
 def main():
     """Docstring."""
-    make_dicts()
-    pygame_init(make_dicts())
+    pygame_init(make_random_dict())
+
 
 if __name__ == '__main__':
     main()
